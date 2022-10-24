77a7b9d348ea97ac9dd5618759008f8ffa4a2e86
Huy <97140326+cut113@users.noreply.github.com>
2022-10-24 15:55:09 +0700

    Intro. to Redux

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -71,56 +71,56 @@ class Contact extends Component {
         if (this.state.touched.telnum && !reg.test(telnum))
             errors.telnum = 'Tel. Number should contain only numbers';
 
-        if(this.state.touched.email && email.split('').filter(x => x === '@').length !== 1)
+        if (this.state.touched.email && email.split('').filter(x => x === '@').length !== 1)
             errors.email = 'Email should contain a @';
 
         return errors;
     }
-        render(){
-            const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
-            return (
-                <div className="container">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Contact us</BreadcrumbItem>
-                    </Breadcrumb>
+    render() {
+        const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
+        return (
+            <div className="container">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Contact us</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Contact us</h3>
+                    <hr />
+                </div>
+                <div className="row row-content">
                     <div className="col-12">
-                        <h3>Contact us</h3>
-                        <hr />
+                        <h3>Location Information</h3>
                     </div>
-                    <div className="row row-content">
-                        <div className="col-12">
-                            <h3>Location Information</h3>
-                        </div>
-                        <div className="col-12 col-sm-4 offset-sm-1">
-                            <h5>Our Address</h5>
-                            <address>
-                                121, Clear Water Bay Road<br />
-                                Clear Water Bay, Kowloon<br />
-                                HONG KONG<br />
-                                <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                                <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                                <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
-                            </address>
-                        </div>
-                        <div className="col-12 col-sm-6 offset-sm-1">
-                            <h5>Map of our Location</h5>
-                        </div>
-                        <div className="col-12 col-sm-11 offset-sm-1">
-                            <div className="btn-group" role="group">
-                                <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                                <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-                                <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
-                            </div>
+                    <div className="col-12 col-sm-4 offset-sm-1">
+                        <h5>Our Address</h5>
+                        <address>
+                            121, Clear Water Bay Road<br />
+                            Clear Water Bay, Kowloon<br />
+                            HONG KONG<br />
+                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                        </address>
+                    </div>
+                    <div className="col-12 col-sm-6 offset-sm-1">
+                        <h5>Map of our Location</h5>
+                    </div>
+                    <div className="col-12 col-sm-11 offset-sm-1">
+                        <div className="btn-group" role="group">
+                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
                         </div>
                     </div>
-                    <div className="row row-content">
-                   <div className="col-12">
-                      <h3>Send us your Feedback</h3>
-                   </div>
+                </div>
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h3>Send us your Feedback</h3>
+                    </div>
                     <div className="col-12 col-md-9">
                         <Form onSubmit={this.handleSubmit}>
-                        <FormGroup row>
+                            <FormGroup row>
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
                                     <Input type="text" id="firstname" name="firstname"
@@ -173,7 +173,7 @@ class Contact extends Component {
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
-                                <Col md={{size: 6, offset: 2}}>
+                                <Col md={{ size: 6, offset: 2 }}>
                                     <FormGroup check>
                                         <Label check>
                                             <Input type="checkbox"
@@ -184,10 +184,10 @@ class Contact extends Component {
                                         </Label>
                                     </FormGroup>
                                 </Col>
-                                <Col md={{size: 3, offset: 1}}>
+                                <Col md={{ size: 3, offset: 1 }}>
                                     <Input type="select" name="contactType"
-                                            value={this.state.contactType}
-                                            onChange={this.handleInputChange}>
+                                        value={this.state.contactType}
+                                        onChange={this.handleInputChange}>
                                         <option>Tel.</option>
                                         <option>Email</option>
                                     </Input>
@@ -203,7 +203,7 @@ class Contact extends Component {
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
-                                <Col md={{size: 10, offset: 2}}>
+                                <Col md={{ size: 10, offset: 2 }}>
                                     <Button type="submit" color="primary">
                                         Send Feedback
                                     </Button>
@@ -211,11 +211,11 @@ class Contact extends Component {
                             </FormGroup>
                         </Form>
                     </div>
-               </div>
                 </div>
-            );
-        }
-
+            </div>
+        );
     }
+
+}
 
 export default Contact;
