b8690a9239c0e43c867dd24108a0a05cb141f775
Huy <97140326+cut113@users.noreply.github.com>
2022-10-24 16:47:05 +0700

    React Redux Form Validation 2

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -17,10 +17,10 @@ class Contact extends Component {
     }
 
 
-    handleSubmit(event) {
-        console.log('Current State is: ' + JSON.stringify(this.state));
-        alert('Current State is: ' + JSON.stringify(this.state));
-        event.preventDefault();
+    handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        //event.preventDefault();
     }
 
     render() {
